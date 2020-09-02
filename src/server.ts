20d59eb719ec33e3025396c385cc1aa42210caf4
rulyox <email@rulyox.com>
2020-09-02 12:19:50 +0900

    Added error handler

--- src/server.ts
+++ src/server.ts
@@ -14,4 +14,10 @@ app.use(bodyParser.json());
 // route
 app.use('/api', API);
 
+// error handler
+app.use((error: Error, request: express.Request, response: express.Response, next: express.NextFunction) => {
+    utility.print(`Error\n${error}`);
+    response.status(500).end();
+});
+
 app.listen(port, () => utility.print(`Server listening on port ${port}...`));
